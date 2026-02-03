6e97f38668de3eada0339952164838b09f402375
Sergey Kosykh <s3rkosal@gmail.com>
2026-02-04 01:54:18 +0400

    added jinja2 templates

--- main.py
+++ main.py
@@ -13,5 +13,21 @@ templates = Jinja2Templates(directory="templates")
 async def index(req: Request):
     
     return templates.TemplateResponse(
-        request=req, name='index.html', context={}
+        request=req, name='index.html', context={"title": "Index page"}
+    )
+    
+@app.get("/sse/", response_class=HTMLResponse)
+async def sse(req: Request):
+    
+    return templates.TemplateResponse(
+        request=req, 
+        name='sse.html', 
+        context={"title": "Server-sent events demo"}
+    )
+    
+@app.get("/ws", response_class=HTMLResponse)
+async def ws(req: Request):
+    
+    return templates.TemplateResponse(
+        request=req, name='ws.html', context={"title": "Web-Sockets demo"}
     )
